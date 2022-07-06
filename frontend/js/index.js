dff7e7f7c97d5e6098de359f09641015a9c0f01d
Nicolas Cartagena <nicolas.a.cartagena.m@gmail.com>
2022-07-06 15:44:32 -0400

    integracion de products api al frontend

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,13 +1,37 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Funcion para cargar las categorias url: /api/category
     cargarCartegorias();
+
+    // Función para cargar los productos url: /api/product
+    cargarProductos();
 });
+
+const cargarProductos = async () => {
+    await fetch('http://localhost:3000/api/product')
+        .then(response => response.json())
+        .then(data => {
+            let products = '';
+            for(let item of data.results) {
+                products += `
+                <div class="card">
+                    <img src='${item.url_image}' class="card-img-top" alt=""/>
+                    <div class="card-body">
+                        <h5 class="card-title">${item.name}</h5>
+                        <h6 class="card-subtitle mb-2">$${item.price} <span class="text-muted">${item.discount}</span></h6>
+                        <p class="card-text">${item.Category.name}</p>
+                        <a href="#" class="card-link"><i class="fa-solid fa-cart-arrow-down"></i></a>
+                    </div>
+                </div>
+                `;
+            }
+            document.getElementById('productos').innerHTML = products;
+        })
+}
 
 const cargarCartegorias = async () => {
     await fetch('http://localhost:3000/api/category')
         .then(response => response.json())
         .then(data =>{
-            console.log(data.results)
             let categoria = '';
             for(let item of data.results) {
                 categoria += `
